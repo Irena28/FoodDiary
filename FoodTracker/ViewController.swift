9de6f9f329f7e25e7cbcd35d15e1ae9c11445fed
Irena Davy <irenadavy@ronald.local>
2015-09-29 17:09:27 -0400

    latest version

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK Proporties
 
@@ -16,16 +16,48 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mealNameLabel: UILabel!
     
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Mark UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
 
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+        
+    }
+    
+    //Mark Actions
+    
+    @IBAction func setDefaultLabelText(sender: UIButton)
+    
+    {
+        mealNameLabel.text = "Default Text"
+    }
 
+    @IBAction func selectImageFromPhotoLibrary(sender: UITapGestureRecognizer)
+    {
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerCotroller = UIImagePickerController()
+        imagePickerCotroller.sourceType = .PhotoLibrary
+        imagePickerCotroller.delegate = self
+        presentViewController(imagePickerCotroller, animated: true, completion: nil)
+    }
 }
 
